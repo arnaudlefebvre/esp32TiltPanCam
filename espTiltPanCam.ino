06ab59f355c38d8f3c51133c581720401d0035cd
Arnaud Lefebvre <arnd.lefebvre@gmail.com>
2021-01-09 19:08:49 +0100

    handle max and margin for servos and pimp angle

--- espTiltPanCam.ino
+++ espTiltPanCam.ino
@@ -83,34 +83,41 @@ const char* password = "C4E6A360";
 
 #include "esp32-hal-ledc.h"
 
-//PINS
-int SFAKE = 13; 
+//Servos PINS
 int SROTATE = 12; 
 int STILT = 14;      
-//CHANNELS
+//Servos CHANNELS
 int CROTATE = 4;
 int CTILT = 5;
-//fq
+//Servos fq
 int FROTATE = 50;
 int FTILT = 50;
 
+//Servos current pos
 int rpos = -1;
 int tpos = -1;
 
-int MAXTILT = 64;
+//Max tilt & Rotate angle
+int MAXTILT = 50;
+int MAXTILT = 180;
 
+//Margin tilt & rotate
+int MARGINTILT = 10;
+int MARGINROTATE = 0;
+
+//Dynamic Servos Pos
 extern int CAMTILT, CAMROTATE, CAMFAKE;
 
 void startCameraServer();
 
 void initChannels() {
-  ledcSetup(CROTATE, FROTATE, TIMER_WIDTH); // channel 1, 50 Hz, 16-bit width   
-  ledcSetup(CTILT, FTILT, TIMER_WIDTH); // channel 2, 50 Hz, 16-bit width  
+  ledcSetup(CROTATE, FROTATE, TIMER_WIDTH);  
+  ledcSetup(CTILT, FTILT, TIMER_WIDTH);  
 }
 
 void initServos() {
-  ledcAttachPin(STILT, CTILT);   // GPIO 0 assigned to channel 2
-  ledcAttachPin(SROTATE, CROTATE);   // GPIO 16 assigned to channel 1
+  ledcAttachPin(STILT, CTILT);   
+  ledcAttachPin(SROTATE, CROTATE);   
 }
 
 void resServos() {
@@ -119,7 +126,7 @@ void resServos() {
 }
 
 void initCam() {
-    camera_config_t config;
+  camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
   config.pin_d0 = Y2_GPIO_NUM;
@@ -158,8 +165,7 @@ void initCam() {
   }
   //drop down frame size for higher initial frame rate
   sensor_t * s = esp_camera_sensor_get();
-  //s->set_framesize(s, FRAMESIZE_QVGA);
-  s->set_framesize(s, FRAMESIZE_UXGA);//v12345vtm
+  s->set_framesize(s, FRAMESIZE_UXGA);
 }
 
 void initWifi() {
@@ -171,6 +177,7 @@ void initWifi() {
   Serial.println("");
   Serial.println("WiFi connected");
 }
+
 
 void setup() {
   pinMode(STILT, OUTPUT);
@@ -197,24 +204,29 @@ void setup() {
 
 void loop() {
   doServo();
-  // put your main code here, to run repeatedly:
   delay(500);
 }
 
 void doServo() {
    Serial.print("CAMROTATE : ");Serial.print(CAMROTATE);Serial.print("CAMTILT : ");Serial.println(CAMTILT);
-   if (CAMTILT > MAXTILT) {
-      CAMTILT = MAXTILT;
+   if (CAMTILT > MAXTILT + MARGINTILT) {
+      CAMTILT = MAXTILT + MARGINTILT;
+   } else if (CAMTILT < MARGINTILT) {
+	   CAMTILT = MARGINTILT;
+   }   
+   if (CAMROTATE > MAXROTATE + MARGINROTATE) {
+      CAMROTATE = MAXROTATE + MARGINROTATE;
+   } else if (CAMROTATE < MARGINROTATE) {
+	   CAMROTATE = MARGINROTATE;
    }
+   Serial.print("CAMROTATE : ");Serial.print(CAMROTATE);Serial.print("CAMTILT : ");Serial.println(CAMTILT);
    int cr = map(CAMROTATE,0,180,COUNT_LOW,COUNT_HIGH);
    int ct = map(CAMTILT,0,180,COUNT_LOW,COUNT_HIGH);       
    Serial.print("cr : ");Serial.print(cr);Serial.print("ct : ");Serial.println(ct);
-   //if ((rpos == -1 && tpos == -1) || (rpos != -1 && tpos != -1 && rpos != cr && tpos != ct)) {
-     //initServos();
+   if ((rpos == -1 && tpos == -1) || (rpos != -1 && tpos != -1 && rpos != cr && tpos != ct)) {
      ledcWrite(CROTATE, cr);
      ledcWrite(CTILT, ct);
-     //resServos();
      rpos = cr;
      tpos = ct;
-   //}
+   }
 }
